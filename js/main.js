eb1a8840d835b132f002708d1f4f4b6370666e41
Tima <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2021-06-02 06:58:22 -0700

    Slick,Changed Colors, Body
    - Started working with body
    - Added slick library to jump between blocks with sliders
    - Changed from yellow to blue borders, bg etc

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+        infinity: false
+    });
+
+});
